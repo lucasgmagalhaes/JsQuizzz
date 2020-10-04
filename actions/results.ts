ff24786df501dabcbb96588f3f7f5e2f04f470ce
Lucas Gomes <lucasgsm88@gmail.com>
2020-10-04 19:20:17 -0300

    fix: results

--- actions/results.ts
+++ actions/results.ts
@@ -1,38 +1,88 @@
-/**
- * @type HTMLButtonElement
- */
-const btnRepeat = document.getElementById("repeatTest");
-/**
- * @type HTMLButtonElement
- */
-const btnIndexPage = document.getElementById("initialScreen");
-/**
- * @type HTMLSpanElement
- */
-const summary = document.getElementById("summary");
+const summary = document.getElementById("summary") as HTMLSpanElement;
+const questionsContainer = document.getElementById("options-container");
 
-if (btnRepeat) {
-  btnRepeat.addEventListener("click", () => {
+function loadResults() {
+  const user = localStorage.getItem("user");
+  const questions = JSON.parse(localStorage.getItem("questions")) as Question[];
+  const ans = JSON.parse(localStorage.getItem("ans"));
+  const hits = calcHitsPercentage(questions, ans);
+  summary.innerHTML = `${user} você acertou ${hits}% das respostas.`;
+  addResponseToDOM(questions, ans);
+}
+
+function calcHitsPercentage(questions: Question[], ans: PickedAnswer[]) {
+  let hit = 0;
+  questions.forEach((question) => {
+    const pickedOption = ans.find(
+      (answer) => answer.questionId === question.id
+    );
+
+    if (pickedOption.answerChosen === question.answerPosition) {
+      hit++;
+    }
+  });
+
+  return (hit * 100) / questions.length;
+}
+
+function addResponseToDOM(questions: Question[], ans: PickedAnswer[]) {
+  questions.forEach((question) => {
+    const resultContainer = document.createElement("div");
+    resultContainer.classList.add("result-question");
+
+    const questionNameLabel = document.createElement("span");
+    questionNameLabel.classList.add("question-name");
+    questionNameLabel.innerText = question.name;
+    resultContainer.appendChild(questionNameLabel);
+
+    for (let i = 0; i <= question.options.length - 1; i++) {
+      const optionLabel = document.createElement("span");
+      optionLabel.classList.add("option");
+      optionLabel.innerText = question.options[i];
+
+      const pickedOption = ans.find(
+        (answer) => answer.questionId === question.id
+      );
+
+      if (pickedOption.answerChosen === question.answerPosition) {
+        optionLabel.classList.add("correct");
+      } else {
+        optionLabel.classList.add("wrong");
+      }
+
+      if (pickedOption.answerChosen == i) {
+        optionLabel.classList.add("picked");
+      }
+
+      resultContainer.appendChild(optionLabel);
+    }
+    questionsContainer.appendChild(resultContainer);
+  });
+}
+
+function addRepeatTestButton() {
+  const repeatTestBtn = document.createElement("button");
+  repeatTestBtn.classList.add("repeat");
+  repeatTestBtn.innerText = "Repetir teste";
+  repeatTestBtn.addEventListener("click", () => {
     localStorage.clear();
     localStorage.setItem("question", "0");
     redirect("question.html", container);
   });
+  questionsContainer.appendChild(repeatTestBtn);
 }
 
-if (btnIndexPage) {
-  btnIndexPage.addEventListener("click", () => {
+function addBackToInitialScreenButton() {
+  const backToInitialScreenBtn = document.createElement("button");
+  backToInitialScreenBtn.classList.add("initialScreen");
+  backToInitialScreenBtn.innerText = "Voltar para tela inicial";
+  backToInitialScreenBtn.addEventListener("click", () => {
     redirect("index.html", container);
   });
+
+  questionsContainer.appendChild(backToInitialScreenBtn);
 }
 
-function loadResults() {
-    const user = localStorage.getItem("user");
-    const questions = JSON.parse(localStorage.getItem("questions")) as Question[];
-    const ans = JSON.parse(localStorage.getItem("ans"));
-    calcHitsPercentage(questions, ans);
-    summary.innerHTML = `${user} você acertou {valor}% das respostas.`;
-}
-
-function calcHitsPercentage(questions: Question[], ans: PickedAnswer[]) {
-
-}
+loadResults();
+addRepeatTestButton();
+addBackToInitialScreenButton();
--- dist/results.js
+++ dist/results.js
@@ -1,34 +1,71 @@
-/**
- * @type HTMLButtonElement
- */
-var btnRepeat = document.getElementById("repeatTest");
-/**
- * @type HTMLButtonElement
- */
-var btnIndexPage = document.getElementById("initialScreen");
-/**
- * @type HTMLSpanElement
- */
 var summary = document.getElementById("summary");
-if (btnRepeat) {
-    btnRepeat.addEventListener("click", function () {
-        localStorage.clear();
-        localStorage.setItem("question", "0");
-        redirect("question.html", container);
-    });
-}
-if (btnIndexPage) {
-    btnIndexPage.addEventListener("click", function () {
-        redirect("index.html", container);
-    });
-}
+var questionsContainer = document.getElementById("options-container");
 function loadResults() {
     var user = localStorage.getItem("user");
     var questions = JSON.parse(localStorage.getItem("questions"));
     var ans = JSON.parse(localStorage.getItem("ans"));
-    calcHitsPercentage(questions, ans);
-    summary.innerHTML = user + " voc\u00EA acertou {valor}% das respostas.";
+    var hits = calcHitsPercentage(questions, ans);
+    summary.innerHTML = user + " voc\u00EA acertou " + hits + "% das respostas.";
+    addResponseToDOM(questions, ans);
 }
 function calcHitsPercentage(questions, ans) {
+    var hit = 0;
+    questions.forEach(function (question) {
+        var pickedOption = ans.find(function (answer) { return answer.questionId === question.id; });
+        if (pickedOption.answerChosen === question.answerPosition) {
+            hit++;
+        }
+    });
+    return (hit * 100) / questions.length;
 }
+function addResponseToDOM(questions, ans) {
+    questions.forEach(function (question) {
+        var resultContainer = document.createElement("div");
+        resultContainer.classList.add("result-question");
+        var questionNameLabel = document.createElement("span");
+        questionNameLabel.classList.add("question-name");
+        questionNameLabel.innerText = question.name;
+        resultContainer.appendChild(questionNameLabel);
+        for (var i = 0; i <= question.options.length - 1; i++) {
+            var optionLabel = document.createElement("span");
+            optionLabel.classList.add("option");
+            optionLabel.innerText = question.options[i];
+            var pickedOption = ans.find(function (answer) { return answer.questionId === question.id; });
+            if (pickedOption.answerChosen === question.answerPosition) {
+                optionLabel.classList.add("correct");
+            }
+            else {
+                optionLabel.classList.add("wrong");
+            }
+            if (pickedOption.answerChosen == i) {
+                optionLabel.classList.add("picked");
+            }
+            resultContainer.appendChild(optionLabel);
+        }
+        questionsContainer.appendChild(resultContainer);
+    });
+}
+function addRepeatTestButton() {
+    var repeatTestBtn = document.createElement("button");
+    repeatTestBtn.classList.add("repeat");
+    repeatTestBtn.innerText = "Repetir teste";
+    repeatTestBtn.addEventListener("click", function () {
+        localStorage.clear();
+        localStorage.setItem("question", "0");
+        redirect("question.html", container);
+    });
+    questionsContainer.appendChild(repeatTestBtn);
+}
+function addBackToInitialScreenButton() {
+    var backToInitialScreenBtn = document.createElement("button");
+    backToInitialScreenBtn.classList.add("initialScreen");
+    backToInitialScreenBtn.innerText = "Voltar para tela inicial";
+    backToInitialScreenBtn.addEventListener("click", function () {
+        redirect("index.html", container);
+    });
+    questionsContainer.appendChild(backToInitialScreenBtn);
+}
+loadResults();
+addRepeatTestButton();
+addBackToInitialScreenButton();
 //# sourceMappingURL=results.js.map
